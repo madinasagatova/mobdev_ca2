07607f7325f435fc9691d7e5dc341f7612b696aa
madinasagatova <2021255@student.cct.ie>
2022-04-22 11:32:51 +0000

    people&planet.page.ts changes

--- src/app/pages/people/people.page.ts
+++ src/app/pages/people/people.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-people',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people.page.scss'],
 })
 export class PeoplePage implements OnInit {
+  people: Observable<any>;
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.people = this.http.get('https://swapi.dev/api/people');
   }
-
+  openDetails(person) {
+    let split = person.url.split('/');
+    let personId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/people/${personId}`);
+  }
 }
--- src/app/pages/planets/planets.page.ts
+++ src/app/pages/planets/planets.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';;
 
 @Component({
   selector: 'app-planets',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planets.page.scss'],
 })
 export class PlanetsPage implements OnInit {
-
-  constructor() { }
+  planets: Observable<any>;
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.planets = this.http.get('https://swapi.dev/api/planets');
   }
-
+  openDetails(planet) {
+    let split = planet.url.split('/');
+    let planetId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/planets/${planetId}`);
+  }
 }
