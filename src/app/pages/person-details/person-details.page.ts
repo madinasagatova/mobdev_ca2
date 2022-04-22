590f3ad11effcbb1f94f414a56ba3b3e68452031
madinasagatova <2021255@student.cct.ie>
2022-04-22 11:52:45 +0000

    person&planet details .ts changes

--- src/app/pages/person-details/person-details.page.ts
+++ src/app/pages/person-details/person-details.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-person-details',
   templateUrl: './person-details.page.html',
   styleUrls: ['./person-details.page.scss'],
 })
 export class PersonDetailsPage implements OnInit {
-
-  constructor() { }
+  person: any;
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://swapi.dev/api/people/${id}`).subscribe(res => {
+      this.person = res;
+    });
   }
 
 }
